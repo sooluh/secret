84092b09cc806deae613e5abb81f9440f7055957
Sool Uh! <suluh.webdevelopers@hotmail.com>
2022-04-22 12:44:17 +0700

    :bricks: fix padding components

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react'
-import { Box, Stack, Text } from '@chakra-ui/react'
+import { Box, Spacer, Stack, Text } from '@chakra-ui/react'
 import moment from 'moment'
 
 const Card: FC<{
@@ -14,7 +14,7 @@ const Card: FC<{
     <>
       <Box w="full" p={4} rounded="xl" backgroundColor={background}>
         <Stack direction="column">
-          <Box mb={2}>
+          <Box>
             <Text>{message}</Text>
             <Text fontSize="xs" textColor="gray.500">
               {date}
@@ -22,18 +22,22 @@ const Card: FC<{
           </Box>
 
           {childrens.length > 0 ? (
-            <Stack spacing="2" direction="column-reverse">
-              {childrens.map(child => (
-                <Box
-                  key={'replies-' + child.id}
-                  borderLeft="2px"
-                  borderColor="gray.500"
-                  pl={3}
-                >
-                  <Text>{child.message}</Text>
-                </Box>
-              ))}
-            </Stack>
+            <>
+              <Spacer />
+
+              <Stack spacing="2" direction="column-reverse">
+                {childrens.map(child => (
+                  <Box
+                    key={'replies-' + child.id}
+                    borderLeft="2px"
+                    borderColor="gray.500"
+                    pl={3}
+                  >
+                    <Text>{child.message}</Text>
+                  </Box>
+                ))}
+              </Stack>
+            </>
           ) : null}
         </Stack>
       </Box>
